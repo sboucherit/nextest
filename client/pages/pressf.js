3a65fe8559e34030c0ee5c920c552670c50660b7
Samy Boucherit <sboucherit@palo-it.com>
2019-07-11 07:48:39 +0000

    code refactoring

--- client/pages/pressf.js
+++ client/pages/pressf.js
@@ -11,9 +11,10 @@ import Web3 from 'web3';
 
 //const Web3 = require('web3');
 //const web3 = new Web3('http://localhost:7545');
+
+//------------------------- WEB3JS INITALIZATION -------------------------\\
+
 let web3;
-
-
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   // We are in the browser and metamask is running.
@@ -31,172 +32,59 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new Web3(provider);
 }
 
+//------------------------- END OF WEB3JS INITALIZATION -------------------------\\
+
+
+let chatMsgLog;
+let currentChatLog = [];
+
+//------------------------- EVENT SUBSCRIPTION -------------------------\\
+
+const subscription = web3.eth.subscribe('logs', {
+  fromBlock: 0
+}, (error, result) => {
+  if (!error) {
+    // console.log("le resultat de la subscription : ", result);
+    console.log("subscription passed");
+    console.log("ID de la subscription : ", result.id)
+  } else {
+    console.error("l'erreur : ", error);
+  }
+})
+  .on("data", (log) => {
+
+    console.log("APPEL 1 FOIS")
+
+    //TMP OUT :
+    /*      
+    console.log("le log des nouvelles data de la subscription : ", log);
+      console.log("ok la on va parser : ", JSON.stringify(accounts, null, 4), "GROBEL", web3.utils.hexToAscii(log.data));
+    
+      messageSender = JSON.stringify(accounts, null, 4);
+    chatMsgLog = web3.utils.hexToAscii(log.data);
+    
+      currentChatLog.push([...currentChatLog, {
+        messageSender
+      },
+      {
+        chatMsgLog
+      }]);
+    
+      currentChatLog.push[(newMessage)];
+    */
+
+  })
+
+
+
 
 
 //Beware things are hardcoded :
 
 const PressfPage = () => {
 
-  // HARDCODED ABI :
+  //------------------------- HARDCODED ABI -------------------------\\
 
-
-
-let abi = [
-  {
-    "constant": false,
-    "inputs": [
-      {
-        "name": "message",
-        "type": "string"
-      }
-    ],
-    "name": "setmessage",
-    "outputs": [
-      {
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": false,
-        "name": "message",
-        "type": "string"
-      },
-      {
-        "indexed": false,
-        "name": "msgAddress",
-        "type": "address"
-      }
-    ],
-    "name": "newMessage",
-    "type": "event"
-  }
-]
-
-//HARDCODED CONTRACT ADDRESS
-0xe80F72a6564e2015Fe51Ce03D15f189BABf4dEa2
-//const contractAddress = '0xa14555fa51668f63e82c3c8716da6c3001477cbd';
-//const contractAddress = '0x75E926820d740537F7377785452b9800892370f1';
-const contractAddress = '0xe80F72a6564e2015Fe51Ce03D15f189BABf4dEa2';
-
-  const [newMessage, setNewMessage] = useState("");
-  let currentChatLog = [];
-
-  const handleNewMessage = async (newMessage) => {
-    setNewMessage(newMessage);
-
-
-    const contract = await new web3.eth.Contract(abi, contractAddress);
-
-
-      const accounts = await web3.eth.getAccounts();
-    
-    //console.log('ABI : ', abi);
-    //console.log("Account 0 : ", accounts[0]);
-    //console.log("contrac.method : ", contract.methods);
-    //console.log("parent message :" + newMessage);
-    //console.log("contrat : ", contract);
-    //console.log("contract address : ", contractAddress, "fin du log puis on tate les events");
-
-
-    //We call the EVENT here :
-
-
-    try {
-      await contract.methods.setmessage(newMessage).send({
-        from: accounts[0]
-      });
-    } catch (e) {
-      console.log("event not emmited : ", e);
-    }
-
-
-      //Get past events :
-    await contract.getPastEvents(
-      'allEvents',
-      {
-        fromBlock: 0,
-        toBlock: 'latest'
-      })
-      .then(events => {
-        console.log("event length : ", events.length)
-      })
-      .catch((err) => console.log(err));
-
-
-
-
-
-      const subscription = await web3.eth.subscribe('logs', {
-        fromBlock: 0
-      }, (error, result) => {
-          if (!error) {
-            console.log("le resultat de la subscription : ", result);
-          }else{
-            console.error("l'erreur : ", error);
-          }
-      })
-      .on("data", (log) => {
-        console.log("le log des nouvelles data de la subscription : ", log);
-        console.log("ok la on va parser : ", JSON.stringify(accounts, null, 4), "GROBEL", web3.utils.hexToAscii(log.data));
-        messageSender = JSON.stringify(accounts, null, 4);
-        currentChatLog.push(JSON.stringify(accounts, null, 4), newMessage);
-        //currentChatLog.push[(newMessage)];
-      })
-
-      console.log("les logs du chat : ", currentChatLog);
-
-      //subscription;
-
-      //get all Event :
-/*       contract.events.allEvents()
-      .on('data', (event) => {
-        console.log("resultat des events : ", event);
-        for(let i=0; i < event.length; i++){
-          console.log("le all events : ", allEvents[i]["args"])
-        }
-      })
-      .on('error', console.error);
- */
-
-
-
-
-    //contract.setmessage(newMessage);
-    // alert('A message was sent: ' + newMessage);
-  }
-
-  //END OF HANDLE NEW_MESSAGE
-
-  return (
-
-    <Layout>
-      <ChatBox />
-      <MesssageBox
-        onNewMessage={handleNewMessage}
-        newMessage={newMessage}
-        setNewMessage={setNewMessage}
-      />
-    </Layout>
-  );
-}
-
-
-export default PressfPage;
-
-
-//GANACHE SETUP :
-
-  // HARDCODED ABI :
-
-/*
 
   let abi = [
     {
@@ -236,8 +124,111 @@ export default PressfPage;
       "type": "event"
     }
   ]
-  
-  //HARDCODED CONTRACT ADDRESS
-  const contractAddress = '0xa14555fa51668f63e82c3c8716da6c3001477cbd';
-  
-  */
+
+  //------------------------- HARDCODED CONTRACT ADDRESS -------------------------\\
+
+  const contractAddress = '0xe80F72a6564e2015Fe51Ce03D15f189BABf4dEa2';
+
+  let [newMessage, setNewMessage] = useState("");
+  let [chatHistory, setChatHistory] = useState("");
+
+  let nbEvent;
+
+
+
+  //------------------------- HANDLE NEW MESSAGE -------------------------\\
+
+  const handleNewMessage = async (newMessage) => {
+    setNewMessage(newMessage);
+
+    let messageSender = null;
+
+    const contract = await new web3.eth.Contract(abi, contractAddress);
+    const accounts = await web3.eth.getAccounts();
+
+
+    console.log("APPEL 3 FOIS");
+    //We call the EVENT here :
+
+
+    try {
+      await contract.methods.setmessage(newMessage).send({
+        from: accounts[0]
+      });
+    } catch (e) {
+      console.log("event not emmited : ", e);
+    }
+
+
+    //await getMessageHistory(contract, accounts);
+  }
+
+  //END OF HANDLE NEW_MESSAGE
+
+    //------------------------- GET ALL MESSAGES V2 -------------------------\\
+const getAllMessages = async (contract, accounts) => {
+  console.log("le log des nouvelles data de la subscription : ", log);
+  console.log("ok la on va parser : ", JSON.stringify(accounts, null, 4), "GROBEL", web3.utils.hexToAscii(log.data));
+
+  messageSender = JSON.stringify(accounts, null, 4);
+chatMsgLog = web3.utils.hexToAscii(log.data);
+
+
+}
+
+
+  //------------------------- GET ALL MESSAGES V1 -------------------------\\
+  const getMessageHistory = async (contract, accounts) => {
+
+   // console.log("getmsg consolelog", newMessage);
+
+    //Get past events :
+    await contract.getPastEvents(
+      'allEvents',
+      {
+        fromBlock: 0,
+        toBlock: 'latest'
+      })
+      .then(events => {
+        //console.log("event length : ", events.length)
+        nbEvent = events.length;
+        //console.log("on log ici en dehors : ", chatHistory);
+      })
+      .catch((err) => console.log(err));
+
+    //console.log("les logs du chat : ", currentChatLog);
+
+    //get all Event :
+    /*       contract.events.allEvents()
+          .on('data', (event) => {
+            console.log("resultat des events : ", event);
+            for(let i=0; i < event.length; i++){
+              console.log("le all events : ", allEvents[i]["args"])
+            }
+          })
+          .on('error', console.error);
+     */
+
+
+
+
+    //contract.setmessage(newMessage);
+    // alert('A message was sent: ' + newMessage);
+  }
+
+  //------------------------- FINAL RETURN -------------------------\\
+  return (
+
+    <Layout>
+      <ChatBox />
+      <MesssageBox
+        onNewMessage={handleNewMessage}
+        newMessage={newMessage}
+        setNewMessage={setNewMessage}
+      />
+    </Layout>
+  );
+}
+
+
+export default PressfPage;
